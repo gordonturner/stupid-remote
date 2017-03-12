3eeba61353c4ad24255ba34828a9a29571dd6f23
Gordon Turner <gordon.turner@gmail.com>
2017-03-12 16:48:54 -0400

    Working lirc support, without cec.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var debug = require('debug')('stupid-remote:app');
 
 var index = require('./routes/index');
 var lirc = require('./routes/lirc');
+// var cec = require('./routes/cec');
 var swagger = require('./routes/swagger');
 
 var app = express();
@@ -18,10 +19,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,17 +31,18 @@ app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'
 
 app.use('/', index);
 app.use('/lirc', lirc);
+//app.use('/cec', cec);
 app.use('/swagger', swagger);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
